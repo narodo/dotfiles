e33cf3474c98c508274aa61e313bf19f36a514dc
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2017-01-15 14:10:44 +0100

    zsh: beatify vcs output

--- zsh/colors.zsh
+++ zsh/colors.zsh
@@ -1,4 +1,3 @@
 # define colors
 PROMPT_NORMAL="%F{cyan}"
 VIM_MODE_COLOR="%F{magenta}"
-#PROMPT_COLOR=$PROMPT_NORMAL
--- zsh/dual-line-prompt.zsh
+++ zsh/dual-line-prompt.zsh
@@ -1,13 +1,23 @@
 
 function zle-line-init zle-keymap-select {
     VIM_PROMPT=""
+    VCS_PROMPT=""
     if [[ $KEYMAP == "vicmd" ]]; then
         PROMPT_COLOR=$VIM_MODE_COLOR
         VIM_PROMPT="[NORMAL]"
     else
 	    PROMPT_COLOR=$PROMPT_NORMAL
     fi
+
     zle reset-prompt
+}
+
+function print_vcs_info {
+
+    VCS_INFO_ITEM=(${(s:|:)${vcs_info_msg_0_}})
+    if [[ -n ${VCS_INFO_ITEM} ]]; then
+        echo "[${VCS_INFO_ITEM[1]}:%F{white}${VCS_INFO_ITEM[2]}${PROMPT_COLOR}${VCS_INFO_ITEM[3]}]"
+    fi
 }
 
 zle -N zle-line-init
@@ -15,5 +25,4 @@ zle -N zle-keymap-select
 
 PROMPT='${PROMPT_COLOR}┌[%*]─[%F{white}%~${PROMPT_COLOR}]
 ${PROMPT_COLOR}└─: %F{white}'
-
-RPROMPT='${VIM_MODE_COLOR}$VIM_PROMPT$(prompt_git_info)$(prompt_hg_info)'
+RPROMPT='${PROMPT_COLOR}${PROMPT_COLOR} $(print_vcs_info)'
