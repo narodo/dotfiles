93eec82e8116f5967f77e11ea37158c2af76b75a
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2021-07-28 14:48:45 +0200

    herbstluft: updated scripts to 0.9.3

--- herbstluftwm/panel.sh
+++ herbstluftwm/panel.sh
@@ -23,8 +23,9 @@ y=${geometry[1]}
 panel_width=${geometry[2]}
 panel_height=16
 font="-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*"
-bgcolor=$(hc get frame_border_normal_color)
-selbg=$(hc get window_border_active_color)
+# extract colors from hlwm and omit alpha-value
+bgcolor=$(hc get frame_border_normal_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
+selbg=$(hc get window_border_active_color|sed 's,^\(\#[0-9a-f]\{6\}\)[0-9a-f]\{2\}$,\1,')
 selfg='#101010'
 
 ####
@@ -34,6 +35,8 @@ if which textwidth &> /dev/null ; then
     textwidth="textwidth";
 elif which dzen2-textwidth &> /dev/null ; then
     textwidth="dzen2-textwidth";
+elif which xftwidth &> /dev/null ; then # For guix
+    textwidth="xftwidth";
 else
     echo "This script requires the textwidth tool of the dzen2 project."
     exit 1
