9a507bba1a1b388a2ea500c8221c03517d31f597
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2021-07-28 21:14:03 +0200

    herbstluft: added app autostart

---
+++ herbstluftwm/autostart_apps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+if hc silent new_attr bool my_not_first_autostart ; then
+    $HOME/.config/herbstluftwm/exec_on_tag.sh 1 google-chrome &
+    $HOME/.config/herbstluftwm/exec_on_tag.sh 9 spotify 
+fi
