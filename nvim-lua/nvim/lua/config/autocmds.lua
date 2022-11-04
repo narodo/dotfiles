d6d24ea4a8fad0edd5994901e861690d3dc63c68
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2022-11-04 15:56:47 +0100

    nvim: fixed autocmd lua filnvim: fixed autocmd lua filee

--- nvim-lua/nvim/lua/config/autocmds.lua
+++ nvim-lua/nvim/lua/config/autocmds.lua
@@ -16,17 +16,6 @@ local aucmd_dict = {
 --autocmd FileType c    set noexpandtab shiftwidth=8 tabstop=8
 --autocmd FileType cpp  set noexpandtab shiftwidth=8 tabstop=8
 --autocmd FileType dts  setlocal noexpandtab shiftwidth=8 tabstop=8
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "dtsi",
--- 	command = "setlocal shiftwidth=2 tabstop=2"
--- })
---
--- vim.cmd([[
---   augroup My_group
---     autocmd!
---     autocmd FileType make set noexpandtab 
---   augroup END
--- ]])
 
 for event, opt_tbls in pairs(aucmd_dict) do
     for _, opt_tbl in pairs(opt_tbls) do
