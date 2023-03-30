324233dd996dd3923ed9bcf01f68bfded9532dca
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2023-03-30 15:03:41 +0200

    nvim: added dap config for codelldb

--- nvim-lua/nvim/lua/config/packages/dap.lua
+++ nvim-lua/nvim/lua/config/packages/dap.lua
@@ -8,7 +8,13 @@ function M.setup()
       id = 'cppdbg',
       type = 'executable',
       command = os.getenv("HOME") .. "/.local/share/nvim/mason/bin/OpenDebugAD7",
+    }
 
+    dap.adapters.codelldb = {
+      id = 'codelldb',
+      type = 'server',
+      host = '127.0.0.1',
+      port = 13000
     }
 
     dap.configurations.cppdbg = {
@@ -34,9 +40,26 @@ function M.setup()
       },
     }
 
-    dap.configurations.c = dap.configurations.cppdbg
-    dap.configurations.cpp = dap.configurations.cppdbg
-    dap.configurations.rust = dap.configurations.cppdbg
+    dap.configurations.codelldb= {
+      {
+        name = "Remote lldb ",
+        type = "codelldb",
+        request = "launch",
+        -- program = function()
+        --   return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        -- end,
+        cwd = '${workspacseFolder}',
+        stopAtEntry = true,
+      },
+      }
+
+    -- dap.configurations.c = dap.configurations.cppdbg
+    -- dap.configurations.cpp = dap.configurations.cppdbg
+    -- dap.configurations.rust = dap.configurations.cppdbg
+
+    dap.configurations.c = dap.configurations.codelldb
+    dap.configurations.cpp = dap.configurations.codelldb
+    dap.configurations.rust = dap.configurations.codelldb
 
 end
 
