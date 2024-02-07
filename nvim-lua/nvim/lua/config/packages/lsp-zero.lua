03808447989f2acb664ab887bdbfb3bfb120ab50
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2024-02-07 19:14:54 +0100

    use clangd instead of ccls

--- nvim-lua/nvim/lua/config/packages/lsp-zero.lua
+++ nvim-lua/nvim/lua/config/packages/lsp-zero.lua
@@ -6,19 +6,19 @@ function M.setup()
 
     lsp.preset('recommended')
 
-    lsp.configure('ccls', {
-      force_setup = true,
-      init_options = {
-        compilationDatabaseDirectory = "build";
-        highlight = {
-          lsRanges = true,
-        };
-      },
-      on_attach = function()
-      end,
-    })
+    -- lsp.configure('ccls', {
+    --   force_setup = true,
+    --   init_options = {
+    --     compilationDatabaseDirectory = "build";
+    --     highlight = {
+    --       lsRanges = true,
+    --     };
+    --   },
+    --   on_attach = function()
+    --   end,
+    -- })
 
-    lsp.skip_server_setup({'clangd'})
+    -- lsp.skip_server_setup({'clangd'})
 
     lsp.setup()
 
@@ -32,26 +32,26 @@ function M.setup()
     })
 
     -- CCLS skip region highlighting
-    local ns = vim.api.nvim_create_namespace('ccls-skip-region')
-    vim.lsp.handlers["$ccls/publishSkippedRanges"] = function(_, result,_ , _)
-        if not result then
-            return
-        else
-            local skipped_ranges = result["skippedRanges"]
-            vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
-            for _ , data in pairs(skipped_ranges) do
-                --
-                -- get lines of each range 
-                -- adjust lines so that #if and #endif are not commented out
-                local start_line=data['start']['line'] +1
-                local end_line=data['end']['line'] -1
-
-                for i =  start_line,end_line, 1 do
-                    vim.api.nvim_buf_add_highlight(0, ns, 'Ignore', i, 0, -1)
-                end
-            end
-        end
-    end
+    -- local ns = vim.api.nvim_create_namespace('ccls-skip-region')
+    -- vim.lsp.handlers["$ccls/publishSkippedRanges"] = function(_, result,_ , _)
+    --     if not result then
+    --         return
+    --     else
+    --         local skipped_ranges = result["skippedRanges"]
+    --         vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+    --         for _ , data in pairs(skipped_ranges) do
+    --             --
+    --             -- get lines of each range 
+    --             -- adjust lines so that #if and #endif are not commented out
+    --             local start_line=data['start']['line'] +1
+    --             local end_line=data['end']['line'] -1
+    --
+    --             for i =  start_line,end_line, 1 do
+    --                 vim.api.nvim_buf_add_highlight(0, ns, 'Ignore', i, 0, -1)
+    --             end
+    --         end
+    --     end
+    -- end
 
 end
 return M
