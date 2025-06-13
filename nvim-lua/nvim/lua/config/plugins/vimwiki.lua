4b91056475a7de2dd0705c39d731c545f4bba155
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2025-06-13 12:34:45 +0200

    nvim: disable vimwiki for now

--- nvim-lua/nvim/lua/config/plugins/vimwiki.lua
+++ nvim-lua/nvim/lua/config/plugins/vimwiki.lua
@@ -1,43 +1,43 @@
 return {
-    'vimwiki/vimwiki',
-    dependencies = {
-        'mattn/calendar-vim',
-    },
-    init = function()
-        local rootPath = '~/tools/vimwiki/'
-        local wiki_work = { path = rootPath .. 'work', name = 'work', ext = '.md' }
-        local wiki_private = { path = rootPath .. '/private', name = 'private', ext = '.md' }
-
-        vim.g.vimwiki_list = { wiki_work, wiki_private }
-
-        local function DailyTemplate()
-            local date = os.date("%Y-%b-%d %H:%M")
-
-            vim.api.nvim_buf_set_lines(0, 0, 0, true, { "= Daily Note: " .. date .. " =" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "== To-Do ==" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "== Notes ==" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-            vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
-            vim.api.nvim_win_set_cursor(0, { 8, 0 })
-        end
-
-        vim.api.nvim_create_autocmd("BufNewFile", {
-            pattern = { "*/diary/[0-9]*.md" },
-            callback = DailyTemplate,
-        })
-
-        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-            pattern = { "*/diary.md" },
-            command = "VimwikiDiaryGenerateLinks",
-        })
-
-        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-            pattern = { "*.md" },
-            command = "lua vim.diagnostic.disable(0)",
-        })
-
-        vim.api.nvim_create_user_command("Diary", "VimwikiDiaryIndex", {})
-    end,
+    -- 'vimwiki/vimwiki',
+    -- dependencies = {
+    --     'mattn/calendar-vim',
+    -- },
+    -- init = function()
+    --     local rootPath = '~/tools/vimwiki/'
+    --     local wiki_work = { path = rootPath .. 'work', name = 'work', ext = '.md' }
+    --     local wiki_private = { path = rootPath .. '/private', name = 'private', ext = '.md' }
+    --
+    --     vim.g.vimwiki_list = { wiki_work, wiki_private }
+    --
+    --     local function DailyTemplate()
+    --         local date = os.date("%Y-%b-%d %H:%M")
+    --
+    --         vim.api.nvim_buf_set_lines(0, 0, 0, true, { "= Daily Note: " .. date .. " =" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "== To-Do ==" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "== Notes ==" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
+    --         vim.api.nvim_buf_set_lines(0, -1, -1, true, { "" })
+    --         vim.api.nvim_win_set_cursor(0, { 8, 0 })
+    --     end
+    --
+    --     vim.api.nvim_create_autocmd("BufNewFile", {
+    --         pattern = { "*/diary/[0-9]*.md" },
+    --         callback = DailyTemplate,
+    --     })
+    --
+    --     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    --         pattern = { "*/diary.md" },
+    --         command = "VimwikiDiaryGenerateLinks",
+    --     })
+    --
+    --     vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    --         pattern = { "*.md" },
+    --         command = "lua vim.diagnostic.disable(0)",
+    --     })
+    --
+    --     vim.api.nvim_create_user_command("Diary", "VimwikiDiaryIndex", {})
+    -- end,
 }
