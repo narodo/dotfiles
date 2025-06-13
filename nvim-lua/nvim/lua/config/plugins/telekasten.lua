aee69f0914052548e9af4d8271b6c7b2cb519198
Immanuel Narodoslawsky <narodo.imm@gmail.com>
2025-06-13 22:56:56 +0200

    nvim: telekasten - add git sync overseer task

--- nvim-lua/nvim/lua/config/plugins/telekasten.lua
+++ nvim-lua/nvim/lua/config/plugins/telekasten.lua
@@ -1,3 +1,5 @@
+local zettelkastenHome = vim.fn.expand("~/zettelkasten")
+
 return {
     'renerocksai/telekasten.nvim',
 
@@ -6,9 +8,10 @@ return {
         'mattn/calendar-vim',
     },
 
+
     config = function()
         require('telekasten').setup({
-            home = vim.fn.expand("~/zettelkasten"), -- Put the name of your notes directory here
+            home = zettelkastenHome,
             take_over_my_home = false,
             auto_set_filetype = false,
             dailies = vim.fn.expand("~/zettelkasten/dailies"),
@@ -16,6 +19,25 @@ return {
             template_new_daily = vim.fn.expand("~/tools/dotfiles/zettelkasten/templates/daily.md"),
             template_new_note = vim.fn.expand("~/tools/dotfiles/zettelkasten/templates/newNote.md"),
         })
+    end,
 
+    init = function()
+        vim.api.nvim_create_user_command("SyncZettelkasten", function(params)
+            local task = require("overseer").new_task({
+                cmd = { "bash" },
+                args = { "-c", "git add * && git commit -m 'Automated commit' && git push" },
+                cwd = zettelkastenHome,
+                components = {
+                    { "mycomps.nvimNotify" },
+                    { "on_output_quickfix", open_on_exit = "failure", open_height = 8 },
+                    "default",
+                },
+            })
+            task:start()
+        end, {
+            desc = "Push zettelkasten home directory to git",
+            nargs = "*",
+            bang = true,
+        })
     end
 }
